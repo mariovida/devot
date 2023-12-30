f57a17ef4b31dabde54c06d10fb74b4121614f11
Mario <mariovidovic97@gmail.com>
2023-12-30 18:13:53 +0100

    history filter updated

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
+
+import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
+import { InputText } from "primereact/inputtext";
 import {
   getFirestore,
   collection,
@@ -10,7 +13,6 @@ import {
   where,
   orderBy,
   getDocs,
-  DocumentData,
 } from "firebase/firestore";
 import styles from "./style/history.module.css";
 
@@ -22,31 +24,31 @@ type Timer = {
 };
 
 const HistoryPage = () => {
-  const [today, setToday] = useState("");
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
   const [previousTimers, setPreviousTimers] = useState<Timer[]>([]);
 
-  useEffect(() => {
-    const getFormattedDate = () => {
-      const currentDate = new Date();
-      const day = currentDate.getDate().toString().padStart(2, "0");
-      const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-      const year = currentDate.getFullYear();
+  const getFormattedDate = (date: Date): string => {
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
 
-      return `${day}.${month}.${year}.`;
-    };
-
-    setToday(getFormattedDate());
-  }, []);
+    return `${day}.${month}.${year}.`;
+  };
 
   const fetchPreviousTimers = async () => {
     const db = getFirestore();
     const trackersCollection = collection(db, "trackers");
 
-    const q = query(
-      trackersCollection,
-      orderBy("entryDate", "desc"),
-      where("entryDate", "<", today)
-    );
+    let q = query(trackersCollection, orderBy("entryDate", "desc"));
+
+    if (startDate) {
+      q = query(q, where("entryDate", ">=", getFormattedDate(startDate)));
+    }
+
+    if (endDate) {
+      q = query(q, where("entryDate", "<=", getFormattedDate(endDate)));
+    }
 
     const querySnapshot = await getDocs(q);
 
@@ -58,22 +60,43 @@ const HistoryPage = () => {
     return previousTimers;
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const timers = await fetchPreviousTimers();
-        setPreviousTimers(timers);
-      } catch (error) {
-        console.error("Error fetching timers:", error);
-      }
-    };
+  const handleFilterClick = async () => {
+    try {
+      const timers = await fetchPreviousTimers();
+      setPreviousTimers(timers);
+    } catch (error) {
+      console.error("Error fetching timers:", error);
+    }
+  };
 
-    fetchData();
-  }, [today]);
+  useEffect(() => {
+    handleFilterClick();
+  }, []);
 
   return (
     <div className={styles.history_wrapper}>
       <h3>Trackers History</h3>
+      <div className={styles.date_filters}>
+        <div>
+          <label>Start Date:</label>
+          <Calendar
+            value={startDate}
+            onChange={(e) => setStartDate(e.value as Date)}
+            showIcon
+            dateFormat="dd.mm.yy"
+          ></Calendar>
+        </div>
+        <div>
+          <label>End Date:</label>
+          <Calendar
+            value={endDate}
+            onChange={(e) => setEndDate(e.value as Date)}
+            showIcon
+            dateFormat="dd.mm.yy"
+          ></Calendar>
+        </div>
+        <Button label="Filter" onClick={handleFilterClick} />
+      </div>
       {previousTimers.length > 0 ? (
         <DataTable
           value={previousTimers}
@@ -90,6 +113,22 @@ const HistoryPage = () => {
             field="description"
             header="Description"
             body={(timer) => timer.description.toString()}
+          ></Column>
+          <Column
+            header="Time Tracked"
+            body={(timer) => {
+              const hours = Math.floor(timer.elapsedTime / 3600);
+              const minutes = Math.floor((timer.elapsedTime % 3600) / 60);
+              const seconds = timer.elapsedTime % 60;
+
+              const formattedTime = `${hours
+                .toString()
+                .padStart(2, "0")}:${minutes
+                .toString()
+                .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+              return formattedTime;
+            }}
           ></Column>
         </DataTable>
       ) : (
